d663018068a5479fa8fd80ae724b61fc7cc06f26
Odell Purifoy <odell@visitdays.com>
2021-01-16 19:59:39 -0500

    Added Profession to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   has_many :speeks, dependent: :destroy
   validates :email, :username, uniqueness: true
   validates :bio, length: { maximum: 500 }, allow_blank: true
+
+  PROFESSIONS = ['Font End', 'Back End', 'Full Stack', 'Dev Ops', 'Cyber Security', 'Artificail Intelligence'].freeze
 end
