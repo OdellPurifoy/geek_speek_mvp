989a6f0200aef9f9b5d615cd42bbe519151f57e9
Odell Purifoy <odell@visitdays.com>
2021-01-20 19:53:02 -0500

    Added action text to speeks

--- app/models/speek.rb
+++ app/models/speek.rb
@@ -2,6 +2,7 @@
 
 class Speek < ApplicationRecord
   belongs_to :user
+  has_rich_text :content
   validates :content, :category, presence: true
 
   CATEGORIES = ['Front End', 'Back End', 'Artificial Intelligence', 'Cyber Security', 'Dev Ops',
