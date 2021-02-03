a43d7782238aa1087cf1d0b9f01c34540e7eafb3
Odell Purifoy <odell@visitdays.com>
2021-02-03 16:10:58 -0500

    Added like feature to speeks

--- app/models/speek.rb
+++ app/models/speek.rb
@@ -3,9 +3,14 @@
 class Speek < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
+  has_many :likes
   has_rich_text :content
   validates :category, presence: true
 
   CATEGORIES = ['Front End', 'Back End', 'Artificial Intelligence', 'Cyber Security', 'Dev Ops',
                 'Full Stack'].freeze
+
+  def liked?(user)
+    !!likes.find { |like| like.user_id == user.id }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_234203) do
+ActiveRecord::Schema.define(version: 2021_02_03_205533) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2021_01_31_234203) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "speek_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["speek_id"], name: "index_likes_on_speek_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "speeks", force: :cascade do |t|
     t.text "content", default: "", null: false
     t.integer "user_id", null: false
@@ -88,5 +97,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_234203) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "speeks"
+  add_foreign_key "likes", "users"
   add_foreign_key "speeks", "users"
 end
